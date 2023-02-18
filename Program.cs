ec45257fbda81762090728bb9150a3715291f4ab
Kelvin Campos <kelvin_campos@ymail.com>
2023-02-17 22:50:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, digite a sua frase:");
+        var frase = Console.ReadLine();
+        System.Console.WriteLine($"Caracteres: {frase.Length}");
+        System.Console.WriteLine($"Palavras: {frase.Split(' ').Length}");
+    }
+}
